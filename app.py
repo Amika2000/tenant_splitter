79377c996fbc60f853893ace75dee9edf1a8fa98
Amika2000 <139161673+Amika2000@users.noreply.github.com>
2026-09-01 10:40:12 +0300

    Update title from 'Individualised' to 'Monthly'

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import tempfile
 
 st.set_page_config(page_title="Tenant PDF Splitter", layout="centered")
 
-st.title("JDJV Individualised Monthly Tenant Invoicing")
+st.title("JDJV Monthly Tenant Invoicing")
 st.write("Upload bulk PDFs and download separated tenant files.")
 
 # -----------------------------
